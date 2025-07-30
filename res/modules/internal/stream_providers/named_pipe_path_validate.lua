d09635d5e07e5ff638206262f4aaf1ff6222ef28
Onran <100285264+Onran0@users.noreply.github.com>
2025-07-30 23:58:09 +0900

    fixed validate for windows

--- res/modules/internal/stream_providers/named_pipe_path_validate.lua
+++ res/modules/internal/stream_providers/named_pipe_path_validate.lua
@@ -1,5 +1,21 @@
+local forbiddenPaths = {
+	"/..\\", "\\../",
+	"/../", "\\..\\"
+}
+
 return function(path)
-	if path:find("/../") or path:find("\\..\\") or path:starts_with("../") or path:starts_with("..\\") then
-		error "special path \"../\" is not allowed in path to named pipe"
+	local corrected = true
+
+	if path:starts_with("../") or path:starts_with("..\\") then
+		corrected = false
+	else
+		for _, forbiddenPath in ipairs(forbiddenPaths) do
+			if path:find(forbiddenPath) then
+				corrected = false
+				break
+			end
+		end	
 	end
+
+	if not corrected then error "special path \"../\" is not allowed in path to named pipe" end
 end
