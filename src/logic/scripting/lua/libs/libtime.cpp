fc6cd003771d3e294ecaefa59a1553dde8b2d5c4
Onran <100285264+Onran0@users.noreply.github.com>
2025-08-01 08:19:47 +0900

    added new time function for work with UTC
    utc_time, utc_offset, local_time

--- src/logic/scripting/lua/libs/libtime.cpp
+++ src/logic/scripting/lua/libs/libtime.cpp
@@ -1,5 +1,6 @@
 #include "engine/Engine.hpp"
 #include "api_lua.hpp"
+#include <ctime>
 
 using namespace scripting;
 
@@ -11,8 +12,49 @@ static int l_delta(lua::State* L) {
     return lua::pushnumber(L, engine->getTime().getDelta());
 }
 
+static int l_utc_time(lua::State* L) {
+    return lua::pushnumber(L, std::time(nullptr));
+}
+
+static int l_local_time(lua::State* L) {
+    std::time_t t = std::time(nullptr);
+
+    std::tm gmt_tm{};
+    gmtime_s(&gmt_tm, &t);
+
+    std::tm local_tm{};
+    localtime_s(&local_tm, &t);
+
+    std::time_t utc_time = std::mktime(&gmt_tm);
+    std::time_t local_time = std::mktime(&local_tm);
+
+    std::time_t offset = local_time - utc_time;
+
+    return lua::pushnumber(L, t + offset);
+}
+
+static int l_utc_offset(lua::State* L) {
+    std::time_t t = std::time(nullptr);
+
+    std::tm gmt_tm{};
+    gmtime_s(&gmt_tm, &t);
+
+    std::tm local_tm{};
+    localtime_s(&local_tm, &t);
+
+    std::time_t utc_time = std::mktime(&gmt_tm);
+    std::time_t local_time = std::mktime(&local_tm);
+
+    std::time_t offset = local_time - utc_time;
+
+    return lua::pushnumber(L, offset);
+}
+
 const luaL_Reg timelib[] = {
     {"uptime", lua::wrap<l_uptime>},
     {"delta", lua::wrap<l_delta>},
+    {"utc_time", lua::wrap<l_utc_time>},
+    {"utc_offset", lua::wrap<l_utc_offset>},
+    {"local_time", lua::wrap<l_local_time>},
     {NULL, NULL}
 };
