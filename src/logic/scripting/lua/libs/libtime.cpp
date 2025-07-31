a6011966692d22435f09e8589bf48f33dc864233
Onran <100285264+Onran0@users.noreply.github.com>
2025-08-01 08:31:34 +0900

    now gmtime_s and localtime_s used only in windows

--- src/logic/scripting/lua/libs/libtime.cpp
+++ src/logic/scripting/lua/libs/libtime.cpp
@@ -4,6 +4,10 @@
 
 using namespace scripting;
 
+#if defined(_WIN32) || defined(_WIN64)
+    #define USE_MSVC_TIME_SAFE
+#endif
+
 static int l_uptime(lua::State* L) {
     return lua::pushnumber(L, engine->getTime().getTime());
 }
@@ -20,14 +24,18 @@ static int l_local_time(lua::State* L) {
     std::time_t t = std::time(nullptr);
 
     std::tm gmt_tm{};
-    gmtime_s(&gmt_tm, &t);
-
     std::tm local_tm{};
+
+#if defined(USE_MSVC_TIME_SAFE)
+    gmtime_s(&gmt_tm, &t);
     localtime_s(&local_tm, &t);
+#else
+    gmtime_r(&t, &gmt_tm);
+    localtime_r(&t, &local_tm);
+#endif
 
     std::time_t utc_time = std::mktime(&gmt_tm);
     std::time_t local_time = std::mktime(&local_tm);
-
     std::time_t offset = local_time - utc_time;
 
     return lua::pushnumber(L, t + offset);
@@ -37,14 +45,18 @@ static int l_utc_offset(lua::State* L) {
     std::time_t t = std::time(nullptr);
 
     std::tm gmt_tm{};
-    gmtime_s(&gmt_tm, &t);
-
     std::tm local_tm{};
+
+#if defined(USE_MSVC_TIME_SAFE)
+    gmtime_s(&gmt_tm, &t);
     localtime_s(&local_tm, &t);
+#else
+    gmtime_r(&t, &gmt_tm);
+    localtime_r(&t, &local_tm);
+#endif
 
     std::time_t utc_time = std::mktime(&gmt_tm);
     std::time_t local_time = std::mktime(&local_tm);
-
     std::time_t offset = local_time - utc_time;
 
     return lua::pushnumber(L, offset);
